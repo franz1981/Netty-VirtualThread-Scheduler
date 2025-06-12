bfd96d9bc512fa0cf75fcd1aa3884a7ad2d0eef2
Francesco Nigro <nigro.fra@gmail.com>
2025-06-12 17:17:58 +0200

    Poller benchmark

---
+++ benchmarks/src/main/java/io/netty/loom/benchmark/PollerBenchmark.java
@@ -0,0 +1,168 @@
+package io.netty.loom.benchmark;
+
+import static io.netty.loom.benchmark.DefaultSchedulerUtils.setupDefaultScheduler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.LockSupport;
+
+import org.openjdk.jmh.annotations.AuxCounters;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+
+import io.netty.loom.LoomSupport;
+import io.netty.util.internal.shaded.org.jctools.queues.MpscUnboundedArrayQueue;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 2, jvmArgs = { "--add-opens=java.base/java.lang=ALL-UNNAMED", "-XX:+UnlockExperimentalVMOptions",
+                             "-XX:-DoJVMTIVirtualThreadTransitions", "-Djdk.trackAllThreads=false" })
+public class PollerBenchmark {
+
+   @Param({ "128000" })
+   public int bytes;
+   @Param({ "0" })
+   public int port;
+   private Queue<Runnable> blockingTasks;
+   private ServerSocket serverAcceptor;
+   private Socket serverSocket;
+   private Socket clientSocket;
+   private InputStream clientIn;
+   private OutputStream serverOut;
+   private long readBytes;
+   private long writtenBytes;
+   private byte[] inData;
+   private byte[] outData;
+   private ThreadFactory vThreadFactory;
+   private Runnable serverWrite;
+   private Runnable clientRead;
+   private volatile Thread carrierParked;
+
+   @AuxCounters(AuxCounters.Type.EVENTS)
+   @State(Scope.Thread)
+   @BenchmarkMode(Mode.Throughput)   public static class SchedulerStats {
+      public long tasksExecuted;
+   }
+
+   static {
+      // this is necessary to make sure the Pooler used sits on a single threaded FJ pool
+      setupDefaultScheduler(1);
+   }
+
+
+   @Setup
+   public void init() throws IOException, InterruptedException, BrokenBarrierException {
+      DefaultSchedulerUtils.validateDefaultSchedulerParallelism(Thread.ofVirtual().factory());
+      serverAcceptor = new ServerSocket(0);
+      var connectionsEstablished = new CyclicBarrier(3);
+      Thread.ofVirtual().start(() -> {
+         try {
+            clientSocket = new Socket("localhost", serverAcceptor.getLocalPort());
+            connectionsEstablished.await();
+         } catch (Throwable e) {
+            throw new RuntimeException(e);
+         }
+      });
+      Thread.ofVirtual().start(() -> {
+         try {
+            serverSocket = serverAcceptor.accept();
+            connectionsEstablished.await();
+         } catch (Throwable e) {
+            throw new RuntimeException(e);
+         }
+      });
+      connectionsEstablished.await();
+      inData = new byte[bytes];
+      outData = new byte[bytes];
+      Arrays.fill(inData, (byte) 42);
+      blockingTasks = new MpscUnboundedArrayQueue<>(1024);
+      vThreadFactory = LoomSupport.setVirtualThreadFactoryScheduler(Thread.ofVirtual(), task -> {
+         blockingTasks.add(task);
+         LockSupport.unpark(carrierParked);
+      }).factory();
+      clientIn = clientSocket.getInputStream();
+      serverOut = serverSocket.getOutputStream();
+      serverWrite = () -> {
+         try {
+            serverOut.write(outData);
+            writtenBytes += bytes;
+         } catch (Throwable e) {
+            throw new RuntimeException(e);
+         }
+      };
+      clientRead = () -> {
+         try {
+            readBytes += clientIn.readNBytes(inData, 0, bytes);
+         } catch (IOException e) {
+            throw new RuntimeException(e);
+         }
+      };
+   }
+
+   @Benchmark
+   public byte[] readWrite(SchedulerStats stats) {
+      // reset counters
+      int bytes = this.bytes;
+      readBytes = 0;
+      writtenBytes = 0;
+      // since we use a FIFO task q, the read should happen first, blocking
+      vThreadFactory.newThread(clientRead).start();
+      vThreadFactory.newThread(serverWrite).start();
+      for (; ; ) {
+         var task = blockingTasks.poll();
+         if (task != null) {
+            stats.tasksExecuted++;
+            task.run();
+         } else {
+            if (readBytes == writtenBytes && readBytes == bytes) {
+               // if the Poller is running within this executor, we still expect that its job is done here!
+               break;
+            }
+            trySleep();
+         }
+      }
+      return inData;
+   }
+
+   private void trySleep() {
+      carrierParked = Thread.currentThread();
+      try {
+         if (blockingTasks.isEmpty()) {
+            LockSupport.park();
+         }
+      } finally {
+         carrierParked = null;
+      }
+   }
+
+
+   @TearDown
+   public void cleanup() throws Exception {
+      clientSocket.close();
+      serverSocket.close();
+      serverAcceptor.close();
+   }
+}
--- benchmarks/src/main/java/io/netty/loom/benchmark/SchedulerBenchmark.java
+++ benchmarks/src/main/java/io/netty/loom/benchmark/SchedulerBenchmark.java
@@ -1,14 +1,13 @@
 package io.netty.loom.benchmark;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
+import static io.netty.loom.benchmark.DefaultSchedulerUtils.setupDefaultScheduler;
+import static io.netty.loom.benchmark.DefaultSchedulerUtils.validateDefaultSchedulerParallelism;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.LongAdder;
 
 import org.HdrHistogram.Histogram;
 import org.openjdk.jmh.annotations.AuxCounters;
@@ -42,14 +41,6 @@ import io.netty.loom.MultithreadVirtualEventExecutorGroup;
                              "-XX:-DoJVMTIVirtualThreadTransitions", "-Djdk.trackAllThreads=false" })
 @State(Scope.Thread)
 public class SchedulerBenchmark {
-
-   private static void setupDefaultScheduler(int parallelism) {
-      int maxPoolSize = Integer.max(parallelism, 256);
-      int minRunnable = Integer.max(parallelism / 2, 1);
-      System.setProperty("jdk.virtualThreadScheduler.parallelism", Integer.toString(parallelism));
-      System.setProperty("jdk.virtualThreadScheduler.maxPoolSize", Integer.toString(maxPoolSize));
-      System.setProperty("jdk.virtualThreadScheduler.minRunnable", Integer.toString(minRunnable));
-   }
 
    static {
       setupDefaultScheduler(1);
@@ -99,7 +90,7 @@ public class SchedulerBenchmark {
          case Default:
             executorGroup = new MultiThreadIoEventLoopGroup(1, factory);
             vtFactory = Thread.ofVirtual().factory();
-            validateDefaultSchedulerParallelism();
+            validateDefaultSchedulerParallelism(vtFactory);
             break;
          default:
             throw new IllegalArgumentException("Unknown threading mode: " + scheduler);
@@ -107,44 +98,6 @@ public class SchedulerBenchmark {
       if (!spinWaitResponse) {
          requestsSemaphore = new Semaphore(0);
       }
-   }
-
-   private void validateDefaultSchedulerParallelism() throws InterruptedException {
-      var sharedCounter = new AtomicLong();
-      var waitToStart = new CyclicBarrier(2);
-      var errors = new LongAdder();
-      var completed = new CountDownLatch(2);
-      // verify serial execution
-      for (int i = 0; i < 2; i++) {
-         vtFactory.newThread(() -> {
-            try {
-               waitToStart.await(10, TimeUnit.SECONDS);
-               if (detectContentionFor(sharedCounter, TimeUnit.SECONDS.toNanos(1))) {
-                  errors.increment();
-               }
-            } catch (Throwable t) {
-               errors.increment();
-            } finally {
-               completed.countDown();
-            }
-         }).start();
-      }
-      completed.await();
-      if (errors.sum() != 0) {
-         throw new IllegalStateException("The default Loom scheduler appear to have too much parallelism: check if the jdk.virtualThreadScheduler.* properties are still valid!");
-      }
-   }
-
-   private static boolean detectContentionFor(AtomicLong sharedCounter, long durationNs) {
-      long start = System.nanoTime();
-      long value = sharedCounter.get();
-      while ((System.nanoTime() - start) < durationNs) {
-         if (!sharedCounter.compareAndSet(value, value + 1L)) {
-            return true;
-         }
-         value++;
-      }
-      return false;
    }
 
    @AuxCounters(AuxCounters.Type.EVENTS)
