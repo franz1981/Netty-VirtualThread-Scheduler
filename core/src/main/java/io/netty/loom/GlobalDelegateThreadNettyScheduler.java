7ebbeec57194ccc9f5ff0877bf2b77d451ef489a
Francesco Nigro <nigro.fra@gmail.com>
2025-11-12 15:29:36 +0100

    chase fibers branch part 1

--- core/src/main/java/io/netty/loom/GlobalDelegateThreadNettyScheduler.java
+++ core/src/main/java/io/netty/loom/GlobalDelegateThreadNettyScheduler.java
@@ -1,6 +1,8 @@
 package io.netty.loom;
 
+import java.lang.invoke.VarHandle;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Global scheduler — provides execution for virtual threads that do not specify their own scheduler.
@@ -18,61 +20,53 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class GlobalDelegateThreadNettyScheduler implements Thread.VirtualThreadScheduler {
 
-   final Thread.VirtualThreadScheduler jdkBuildinScheduler;
+    private static GlobalDelegateThreadNettyScheduler INSTANCE;
 
-   final ConcurrentHashMap<Thread, Thread.VirtualThreadScheduler> internalSchedulerMappings = new ConcurrentHashMap<>();
+    final Thread.VirtualThreadScheduler jdkBuildinScheduler;
 
-   public GlobalDelegateThreadNettyScheduler(Thread.VirtualThreadScheduler jdkBuildinScheduler) {
-      this.jdkBuildinScheduler = jdkBuildinScheduler;
-   }
+    final ConcurrentHashMap<Thread, AtomicReference<VirtualThreadNettyScheduler>> unstartedThreads = new ConcurrentHashMap<>();
 
-   @Override
-   public void execute(Thread vthread, Runnable task) {
-      // When scheduling virtual threads, `GlobalDelegateThreadNettyScheduler::execute` will not be invoked concurrently for the same virtual thread,
-      // so it’s safe to replace `computeIfAbsent` with a `get + put` approach to reduce overhead.
-      Thread.VirtualThreadScheduler internalScheduler = internalSchedulerMappings.get(vthread);
-      if (internalScheduler == null) {
-         internalScheduler = determineScheduler();
-         // we remember ANY mapping, including the JDK built-in scheduler ones
-         internalSchedulerMappings.put(vthread, internalScheduler);
-      }
-      // regardless which scheduler is used, we need to remove the mapping once the vthread is terminated
-      internalScheduler.execute(vthread, () -> {
-         try {
-            task.run();
-         } finally {
-            if (vthread.getState() == Thread.State.TERMINATED) {
-               internalSchedulerMappings.remove(vthread);
+    public GlobalDelegateThreadNettyScheduler(Thread.VirtualThreadScheduler jdkBuildinScheduler) {
+        this.jdkBuildinScheduler = jdkBuildinScheduler;
+        INSTANCE = this;
+        VarHandle.storeStoreFence();
+    }
+
+    @Override
+    public void onStart(Thread.VirtualThreadTask virtualThreadTask) {
+        // TODO this is not great for 2 reasons:
+        // 1. we are doing a remove on a concurrent map even for v threads which are not really interesting to us
+        // 2. if a vThread will never start, it will leak here forever
+        var assignedSchedulerRef = unstartedThreads.remove(virtualThreadTask.thread());
+        if (assignedSchedulerRef == null) {
+            // TODO per carrier sub-pollers goes here, but we want them to inherit the scheduler from the caller context
+        }
+        var scheduler = assignedSchedulerRef != null ? assignedSchedulerRef.get() : null;
+        if (scheduler != null) {
+            // attach the assigned scheduler to the task
+            virtualThreadTask.attach(assignedSchedulerRef);
+            if (scheduler.execute(virtualThreadTask)) {
+                return;
             }
-         }
-      });
-   }
+            // the v thread has been rejected by its assigned scheduler, clean it up and fallback to JDK
+            virtualThreadTask.attach(null);
+        }
+        jdkBuildinScheduler.onStart(virtualThreadTask);
+    }
 
-   private Thread.VirtualThreadScheduler determineScheduler() {
-      Thread callerThread = Thread.currentThread();
-      // platform thread
-      if (!callerThread.isVirtual()) {
-         return jdkBuildinScheduler;
-      }
-      VirtualThreadNettyScheduler current = VirtualThreadNettyScheduler.current();
-      // The current thread was spawned from a specific VirtualThreadNettyScheduler,
-      // so we continue using that scheduler.
-      if (current != null) {
-         return current;
-      }
-      Thread.VirtualThreadScheduler parentScheduler = internalSchedulerMappings.get(callerThread);
-      if (parentScheduler != null) {
-         return parentScheduler;
-      }
+    @Override
+    public void onContinue(Thread.VirtualThreadTask virtualThreadTask) {
 
-      // if the caller thread is not known we assume it won't benefit from inheriting, so its children
-      // will use the JDK built-in scheduler
-      return jdkBuildinScheduler;
-   }
+    }
 
-   // just for benchmark
-   public Thread.VirtualThreadScheduler getJdkBuildinScheduler() {
-      return jdkBuildinScheduler;
-   }
+    // just for benchmark
+    public Thread.VirtualThreadScheduler getJdkBuildinScheduler() {
+        return jdkBuildinScheduler;
+    }
+
+    static Thread assignUnstarted(Thread unstarted, AtomicReference<VirtualThreadNettyScheduler> schedulerRef) {
+        INSTANCE.unstartedThreads.put(unstarted, schedulerRef);
+        return unstarted;
+    }
 }
 
