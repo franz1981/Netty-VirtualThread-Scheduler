a3d838c221f4d43db5bbf0111c043a441e851226
Francesco Nigro <nigro.fra@gmail.com>
2026-01-14 16:16:17 +0100

    Refactor SchedulerHandoffBenchmark to improve event loop thread handling and request processing

--- benchmarks/src/main/java/io/netty/loom/benchmark/SchedulerHandoffBenchmark.java
+++ benchmarks/src/main/java/io/netty/loom/benchmark/SchedulerHandoffBenchmark.java
@@ -16,6 +16,7 @@ package io.netty.loom.benchmark;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.IoEventLoop;
 import io.netty.channel.MultiThreadIoEventLoopGroup;
 import io.netty.channel.epoll.EpollIoHandler;
 import io.netty.channel.nio.NioIoHandler;
@@ -23,13 +24,14 @@ import io.netty.channel.uring.IoUringIoHandler;
 import io.netty.loom.VirtualMultithreadIoEventLoopGroup;
 import io.netty.util.concurrent.FastThreadLocal;
 import org.HdrHistogram.Histogram;
-import org.jctools.queues.MpscBlockingConsumerArrayQueue;
+import org.jctools.queues.MpscArrayQueue;
 import org.jctools.util.Pow2;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.BenchmarkParams;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.concurrent.*;
 import java.util.function.Supplier;
 
@@ -45,7 +47,7 @@ public class SchedulerHandoffBenchmark {
 	}
 
 	// default is 1000 rps per user
-	@Param({"1000"})
+	@Param({"100"})
 	int serviceTimeUs;
 
 	@Param({"512"})
@@ -58,20 +60,17 @@ public class SchedulerHandoffBenchmark {
 	@Param({"100"})
 	int concurrency;
 
-	@Param({"2"})
-	int eventLoopThreads;
+	private static final int EL_THREADS = Integer.getInteger("elThreads", -1);
 
-	@Param({"2"})
-	int fjParallelism;
-
-	@Param({"NIO", "EPOLL"})
+	@Param({"EPOLL"})
 	IO io;
 
 	MultiThreadIoEventLoopGroup group;
 
 	Supplier<ThreadFactory> threadFactory;
 
-	BlockingQueue<byte[]> requestQueue;
+	// let's make
+	Queue<byte[]> requestQueue;
 
 	private static final CopyOnWriteArrayList<Histogram> histograms = new CopyOnWriteArrayList<>();
 
@@ -86,8 +85,8 @@ public class SchedulerHandoffBenchmark {
 
 	@Setup
 	public void setup(BenchmarkParams params) throws ExecutionException, InterruptedException {
-		if (fjParallelism > 0) {
-			DefaultSchedulerUtils.setupDefaultScheduler(fjParallelism);
+		if (EL_THREADS <= 0) {
+			throw new IllegalStateException("Please set the elThreads system property to a positive integer");
 		}
 		var ioFactory = switch (io) {
 			case NIO -> NioIoHandler.newFactory();
@@ -95,17 +94,15 @@ public class SchedulerHandoffBenchmark {
 			case EPOLL -> EpollIoHandler.newFactory();
 		};
 		if (params.getBenchmark().contains("custom")) {
-			var group = new VirtualMultithreadIoEventLoopGroup(
-					eventLoopThreads > 0 ? eventLoopThreads : Runtime.getRuntime().availableProcessors(), ioFactory);
+			var group = new VirtualMultithreadIoEventLoopGroup(EL_THREADS, ioFactory);
 			threadFactory = group::vThreadFactory;
 			this.group = group;
 		} else {
-			group = new MultiThreadIoEventLoopGroup(
-					eventLoopThreads > 0 ? eventLoopThreads : Runtime.getRuntime().availableProcessors(), ioFactory);
+			group = new MultiThreadIoEventLoopGroup(EL_THREADS, ioFactory);
 			var sameFactory = Thread.ofVirtual().factory();
 			threadFactory = () -> sameFactory;
 		}
-		requestQueue = new MpscBlockingConsumerArrayQueue<>(Pow2.roundToPowerOfTwo(concurrency));
+		requestQueue = new MpscArrayQueue<>(Pow2.roundToPowerOfTwo(concurrency));
 		for (int i = 0; i < concurrency; i++) {
 			requestQueue.offer(new byte[requestBytes]);
 		}
@@ -114,20 +111,31 @@ public class SchedulerHandoffBenchmark {
 	@Benchmark
 	@Fork(value = 2, jvmArgs = {"--add-opens=java.base/java.lang=ALL-UNNAMED", "-XX:+UnlockExperimentalVMOptions",
 			"-XX:-DoJVMTIVirtualThreadTransitions", "-Djdk.trackAllThreads=false",
-			"-Djdk.virtualThreadScheduler.implClass=io.netty.loom.NettyScheduler", "-Djdk.pollerMode=3"})
+			"-Djdk.virtualThreadScheduler.implClass=io.netty.loom.NettyScheduler", "-Djdk.pollerMode=3",
+			"-DelThreads=4"})
 	public void customScheduler(Blackhole bh) throws InterruptedException {
 		doRequest(bh);
 	}
 
 	@Benchmark
 	@Fork(value = 2, jvmArgs = {"--add-opens=java.base/java.lang=ALL-UNNAMED", "-XX:+UnlockExperimentalVMOptions",
-			"-XX:-DoJVMTIVirtualThreadTransitions", "-Djdk.trackAllThreads=false"})
-	public void defaultScheduler(Blackhole bh) throws InterruptedException {
+			"-XX:-DoJVMTIVirtualThreadTransitions", "-Djdk.trackAllThreads=false", "-DelThreads=4",
+			"-Djdk.virtualThreadScheduler.parallelism=4"})
+	public void defaultSchedulerFourFjThreads(Blackhole bh) throws InterruptedException {
 		doRequest(bh);
 	}
 
-	private void doRequest(Blackhole bh) throws InterruptedException {
-		byte[] request = requestQueue.take();
+	@Benchmark
+	@Fork(value = 2, jvmArgs = {"--add-opens=java.base/java.lang=ALL-UNNAMED", "-XX:+UnlockExperimentalVMOptions",
+			"-XX:-DoJVMTIVirtualThreadTransitions", "-Djdk.trackAllThreads=false", "-DelThreads=2",
+			"-Djdk.virtualThreadScheduler.parallelism=2"})
+	public void defaultSchedulerTwoFjThreads(Blackhole bh) throws InterruptedException {
+		doRequest(bh);
+	}
+
+	// just burn a full core on this!
+	private void doRequest(Blackhole bh) {
+		byte[] request = spinWaitRequest();
 		long startRequest = System.nanoTime();
 		// write some data into the request
 		Arrays.fill(request, (byte) 1);
@@ -141,46 +149,69 @@ public class SchedulerHandoffBenchmark {
 			}
 			// off-load the actual (blocking) processing to a virtual thread
 			threadFactory.get().newThread(() -> {
-				try {
-					// simulate processing time:
-					// NOTE: if we're using sleep here, the built-in scheduler will use the FJ
-					// built-in one
-					// but the custom scheduler, nope, see
-					// https://github.com/openjdk/loom/blob/3d9e866f60bdebc55b59b9fd40a4898002c35e96/src/java.base/share/classes/java/lang/VirtualThread.java#L1629
-					TimeUnit.MICROSECONDS.sleep(serviceTimeUs);
-					// allocate a response
-					int responseBytes = this.responseBytes;
-					ByteBuf responseData = ByteBufAllocator.DEFAULT.buffer(responseBytes);
-					// write the response content
-					for (int i = 0; i < responseBytes; i++) {
-						responseData.writeByte(42);
-					}
-					el.execute(() -> {
-						// read the response
-						int toRead = this.responseBytes;
-						for (int i = 0; i < toRead; i++) {
-							bh.consume(responseData.getByte(i));
-						}
-						responseData.release();
-						// record RTT
-						long rttNs = System.nanoTime() - startRequest;
-						Histogram histogram = rttHistogram.get();
-						histogram.recordValue(rttNs);
-						// offer it just at the end
-						requestQueue.add(request);
-					});
-				} catch (Throwable e) {
-					throw new RuntimeException(e);
-				}
+				blockingProcess(bh, el, startRequest, request);
 			}).start();
 		});
+	}
+
+	// this is required to make sure NONE of the fine grain ops like writeByte won't
+	// be inlined
+	@CompilerControl(CompilerControl.Mode.DONT_INLINE)
+	private void blockingProcess(Blackhole bh, IoEventLoop el, long startRequest, byte[] request) {
+		try {
+			// simulate processing time:
+			// NOTE: if we're using sleep here, the built-in scheduler will use the FJ
+			// built-in one
+			// but the custom scheduler, nope, see
+			// https://github.com/openjdk/loom/blob/3d9e866f60bdebc55b59b9fd40a4898002c35e96/src/java.base/share/classes/java/lang/VirtualThread.java#L1629
+			TimeUnit.MICROSECONDS.sleep(serviceTimeUs);
+			// allocate a response
+			int responseBytes = this.responseBytes;
+			ByteBuf responseData = ByteBufAllocator.DEFAULT.buffer(responseBytes);
+			// write the response content
+			for (int i = 0; i < responseBytes; i++) {
+				responseData.writeByte(42);
+			}
+			el.execute(() -> {
+				nonBlockingCompleteProcessing(bh, startRequest, request, responseData);
+			});
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	// this is required to make sure NONE of the fine grain ops like writeByte won't
+	// be inlined
+	@CompilerControl(CompilerControl.Mode.DONT_INLINE)
+	private void nonBlockingCompleteProcessing(Blackhole bh, long startRequest, byte[] request, ByteBuf responseData) {
+		// read the response
+		int toRead = this.responseBytes;
+		for (int i = 0; i < toRead; i++) {
+			bh.consume(responseData.getByte(i));
+		}
+		responseData.release();
+		// record RTT
+		long rttNs = System.nanoTime() - startRequest;
+		Histogram histogram = rttHistogram.get();
+		histogram.recordValue(rttNs);
+		// offer it just at the end
+		requestQueue.add(request);
+	}
+
+	private byte[] spinWaitRequest() {
+		byte[] request = requestQueue.poll();
+		while (request == null) {
+			Thread.onSpinWait();
+			request = requestQueue.poll();
+		}
+		return request;
 	}
 
 	@TearDown
 	public void shutdown() throws ExecutionException, InterruptedException {
 		// wait for all tasks to complete
 		for (int i = 0; i < concurrency; i++) {
-			requestQueue.take();
+			spinWaitRequest();
 		}
 		group.shutdownGracefully().get();
 		// print percentiles of RTT
